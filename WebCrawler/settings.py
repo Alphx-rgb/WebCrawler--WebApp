908d39ed0c9f168c9676080589c1a787795608fe
Ayush Budhiraja <ayushbudhiraja795@gmail.com>
2022-07-01 11:49:55 +0530

    bugs fixed
    Signed-off-by: Ayush Budhiraja <ayushbudhiraja795@gmail.com>

--- WebCrawler/settings.py
+++ WebCrawler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5uh*_315rf@73&+oq=it03n5f12)6c)46&ovg@tjl7epkubdr%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -123,10 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'templates/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "templates/"),
+]
